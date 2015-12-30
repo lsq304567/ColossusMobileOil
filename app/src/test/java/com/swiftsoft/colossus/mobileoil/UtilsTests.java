a68745d660668536edfa5ea9e41b199c68c9ce54
alan <T4nger!ne>
2015-12-30 14:51:55 +0000

    Extra Utils.convert2int unit tests

--- app/src/test/java/com/swiftsoft/colossus/mobileoil/UtilsTests.java
+++ app/src/test/java/com/swiftsoft/colossus/mobileoil/UtilsTests.java
@@ -7,14 +7,63 @@ import static org.junit.Assert.assertEquals;
 public class UtilsTests
 {
     @Test
-    public void convert2int_zero_string()
+    public void convert2int_null()
     {
-        assertEquals("Not zero", 0, Utils.Convert2Int("0"));
+        assertEquals("Expected 0 to be returned", 0, Utils.Convert2Int(null));
     }
 
     @Test
-    public void convert2int_one_string()
+    public void convert2int_empty()
+    {
+        assertEquals("Expected 0 to be returned", 0, Utils.Convert2Int(""));
+    }
+
+    @Test
+    public void convert2int_zero()
+    {
+        assertEquals("Not zero", 0, Utils.Convert2Int("0"));
+        assertEquals("Not minus zero", 0, Utils.Convert2Int("-0"));
+    }
+
+    @Test
+    public void convert2int_one()
     {
         assertEquals("Not one", 1, Utils.Convert2Int("1"));
+    }
+
+    @Test
+    public void convert2int_minus_one()
+    {
+        assertEquals("Not minus one", -1, Utils.Convert2Int("-1"));
+    }
+
+    @Test
+    public void convert2int_one_thousand()
+    {
+        assertEquals("Not one thousand", 1000, Utils.Convert2Int("1000"));
+    }
+
+    @Test
+    public void convert2int_one_thousand_comma()
+    {
+        assertEquals("Expected zero to be returned", 0, Utils.Convert2Int("1,000"));
+    }
+
+    @Test
+    public void convert2int_minus_one_thousand()
+    {
+        assertEquals("Not one thousand", -1000, Utils.Convert2Int("-1000"));
+    }
+
+    @Test
+    public void convert2int_minus_one_thousand_comma()
+    {
+        assertEquals("Expected 0 to be returned", 0, Utils.Convert2Int("-1,000"));
+    }
+
+    @Test
+    public void convert2int_decimal_point()
+    {
+        assertEquals("Expected 0 to be returned", 0, Utils.Convert2Int("1.5"));
     }
 }
